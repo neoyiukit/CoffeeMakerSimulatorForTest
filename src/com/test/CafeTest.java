9c11777d9601b730fbf8957b1ca590535b91c123
Neo Yiu <neo.yiu@rakuten.com>
2017-08-14 22:23:27 +0100

    removed Main and added a test case of Espresso

--- src/com/test/CafeTest.java
+++ src/com/test/CafeTest.java
@@ -10,4 +10,16 @@ import org.junit.Test;
  * This is to test the functionality of Cafe class
  */
 public class CafeTest {
+
+    @Test
+    public void canBrewEspresso(){ // to test if the cafe can brew Espresso
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7); // beans needed to be restocked before the test
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        // 1. To test it's an espresso, 2. there is no milk and 3. we've got enough coffee
+        Assert.assertEquals(CoffeeType.Espresso, coffee.getType()); // expected value and how you got it from getType
+        Assert.assertEquals(0, coffee.getMilk());
+        Assert.assertEquals(7, coffee.getBeans());
+    }
 }
