ecc4d177661e442a3350c44fe6a6bd27c994d622
Neo Yiu <neo.yiu@rakuten.com>
2017-08-15 00:24:24 +0100

    added the test for brewCoffeeConsimesBean

--- src/com/coffeeApp/CafeTest.java
+++ src/com/coffeeApp/CafeTest.java
@@ -1,9 +1,7 @@
-package com.test;
+package com.coffeeApp;
 
-import com.coffeeApp.*;
 import junit.framework.Assert;
 import org.junit.Test;
-
 
 /**
  * Created by neo.yiu on 14/08/2017.
@@ -13,13 +11,31 @@ public class CafeTest {
 
     @Test
     public void canBrewEspresso(){ // to test if the cafe can brew Espresso
+
+        // Given
         Cafe cafe = new Cafe();
         cafe.restockBeans(7); // beans needed to be restocked before the test
+
+        // When
         Coffee coffee = cafe.brew(CoffeeType.Espresso);
 
+        // Then
         // 1. To test it's an espresso, 2. there is no milk and 3. we've got enough coffee
         Assert.assertEquals(CoffeeType.Espresso, coffee.getType()); // expected value and how you got it from getType
         Assert.assertEquals(0, coffee.getMilk());
         Assert.assertEquals(7, coffee.getBeans());
+    }
+
+    @Test
+    public void brewCoffeeConsumesBean(){
+        // Given
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(7); // beans needed to be restocked before the test
+
+        // When
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        // Then
+        Assert.assertEquals(0,cafe.getBeansInStock());
     }
 }
